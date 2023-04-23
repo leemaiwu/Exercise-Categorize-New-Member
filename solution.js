c8c1de1c9ae32d1fcc9e8d3dddda777873d7f54b
Laura Wu <leemaiwu@gmail.com>
2023-04-23 13:35:23 -0600

    Completed with for loop and if/else statement

---
+++ solution.js
@@ -0,0 +1,17 @@
+function openOrSenior(data){
+    let category = []
+    for (let i = 0; i < data.length; i++) {
+        if (data[i][0] >= 55 && data[i][1] > 7 && data[i][1] <= 26) {
+            category.push('Senior')
+        }
+        else {
+            category.push('Open')
+        }
+    }
+    return category
+}
+
+
+console.log(openOrSenior([[45, 12],[55, 21],[19, -2],[104, 20]])) // ['Open', 'Senior', 'Open', 'Senior']
+console.log(openOrSenior([[3, 12],[55, 1],[91, -2],[53, 23]])) // ['Open', 'Open', 'Open', 'Open']
+console.log(openOrSenior([[59, 12],[55, -1],[12, -2],[12, 12]])) // ['Senior', 'Open', 'Open', 'Open']
